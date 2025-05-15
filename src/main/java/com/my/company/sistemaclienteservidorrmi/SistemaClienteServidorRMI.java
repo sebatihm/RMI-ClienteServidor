7686fb2a7e9379a56ce628f84edfa36abcac4c29
Sebati HM <sebashermon003@gmail.com>
2025-05-15 16:23:11 -0600

    Main Creada

---
+++ src/main/java/com/my/company/sistemaclienteservidorrmi/SistemaClienteServidorRMI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.my.company.sistemaclienteservidorrmi;
+
+/**
+ *
+ * @author Sebah
+ */
+public class SistemaClienteServidorRMI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
